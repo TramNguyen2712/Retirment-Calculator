20ee012dcc828082a44c782e84fc573499ba5473
TramNguyen2712 <tramnguyen27121996@gmail.com>
2025-02-26 11:29:44 -0600

    Database created

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, render_template, request, Response 
+import sqlite3
+
 
 app = Flask(__name__)
+
+
+sqliteConnection = sqlite3.connect('sql.db')
+cursor = sqliteConnection.cursor()
+print('DB init')
+
 
 @app.route('/')
 def index():
@@ -22,40 +30,81 @@ def retirement_saving(nestEgg,rate,salary,contributed,currentAge,retirementAge,
         
     return totalSaved
 
-def yearly_saving(nestEgg,rate,salary,contributed,currentAge,retirementAge,
+def retirement_summary(nestEgg,rate,salary,contributed,currentAge,retirementAge,
                       salaryIncrease,incomeActiveYears,incomeInActiveYears):
     # Calculate the total amount of money saved by the time of retirement
     totalSaved = nestEgg
     contributed_amount = salary * (contributed/100)
     yearly_saving = []
+    final_salary = salary
     for i in range(currentAge, retirementAge):
+        beginning = totalSaved
         totalSaved += totalSaved * (rate/100)
         totalSaved += contributed_amount
+
+        withdrawn = 0
+        final_salary += final_salary * (salaryIncrease/100)
+        yearly_saving.append({
+            "Age": i,
+            "Beginning Balance": round(beginning,0),
+            "Contribution": round(contributed_amount,0),
+            "Withdrawn": withdrawn,
+            "Ending Balance": round(totalSaved,0)
+        })
+        
         contributed_amount += contributed_amount * (salaryIncrease/100)
-        yearly_saving.append(
-            
-        )
+
+    for i in range(retirementAge,100):
+        beginning = totalSaved
+        contributed_amount = 0
+        if i <= 85:
+            withdrawn = final_salary * incomeActiveYears /100
+        else:
+            withdrawn = final_salary * incomeInActiveYears /100
+
+        totalSaved = totalSaved + totalSaved * (rate/100) - withdrawn
+
+        if totalSaved < 0:
+            totalSaved = 0
+
+        yearly_saving.append({
+            "Age": i,
+            "Beginning Balance": round(beginning,0),
+            "Contribution": contributed_amount,
+            "Withdrawn": round(withdrawn,0),
+            "Ending Balance": round(totalSaved,0)
+        })
+
+        if totalSaved == 0:
+            break
+
+    return yearly_saving
 
 
 @app.route('/calculate',methods=['POST'])
 def calculate():
     # Get the input values from the form
-    nestEgg = float(request.form['nestEgg'])
+    nestEgg = int(request.form['nestEgg'])
     rate = float(request.form['rate'])
-    salary = float(request.form['salary'])
+    salary = int(request.form['salary'])
     contributed = float(request.form['contributed'])
     currentAge = int(request.form['currentAge'])
     retirementAge = int(request.form['retirementAge'])
     salaryIncrease = float(request.form['salaryIncrease'])
-    incomeActiveYears = float(request.form['incomeActiveYears'])
-    incomeInActiveYears = float(request.form['incomeInactiveYears'])
+    incomeActiveYears = int(request.form['incomeActiveYears'])
+    incomeInActiveYears = int(request.form['incomeInactiveYears'])
 
     totalSaved = retirement_saving(nestEgg,rate,salary,contributed,currentAge,retirementAge,
                       salaryIncrease)
     
-    return render_template('retirement.html', totalSaved=f"${totalSaved:,.2f}",
+    yearly_summary = retirement_summary(nestEgg,rate,salary,contributed,currentAge,retirementAge,
+                      salaryIncrease,incomeActiveYears,incomeInActiveYears)
+    
+    return render_template('retirement.html', totalSaved=f"${totalSaved:,.0f}",
                                nestEgg=nestEgg, rate=rate, salary=salary, contributed=contributed, 
-                               currentAge=currentAge, retirementAge=retirementAge, salaryIncrease=salaryIncrease)
+                               currentAge=currentAge, retirementAge=retirementAge, salaryIncrease=salaryIncrease, 
+                               incomeActiveYears = incomeActiveYears, incomeInActiveYears = incomeInActiveYears, 
+                            yearly_summary = yearly_summary)
 
 
 if __name__=='__main__':
